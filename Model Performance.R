29f55c49d7f6e841f615d00ab1282325b4e6b852
drshah96 <drshah96@gmail.com>
2020-06-19 12:50:13 -0400

    Model Performance with laplace equal to 1 and without laplace

---
+++ Model Performance.R
@@ -0,0 +1,17 @@
+###### AUTHOR TEXT CLASSIFICATION #######
+##### Model Performance #####
+
+source("Data Processing.R")
+
+#Naive Bayes
+if(!require(e1071)) install.packages("e1071")
+library(e1071)
+data_classifier <- naiveBayes(data_train, Data_train$Author)
+
+#### Evaluating Model Performance
+data_test_pred <- predict(data_classifier, data_test)
+
+
+##### Improving Model Performance 
+data_classifier2 <- naiveBayes(data_train, Data_train$Author,laplace = 1)
+data_test_pred2 <- predict(data_classifier2, data_test)
