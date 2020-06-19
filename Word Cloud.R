e6f2d7def783f8e657a7abe7056b6aaf59dcd5b8
drshah96 <drshah96@gmail.com>
2020-06-19 12:51:24 -0400

    Appended code to main File

--- Word Cloud.R
+++
@@ -1,8 +0,0 @@
-###### AUTHOR TEXT CLASSIFICATION #######
-##### Word Cloud #####
-
-if(!require(wordcloud)) install.packages("wordcloud")
-library(wordcloud)
-
-wordcloud(Data_test_corpus_clean, min.freq = 40, random.order = FALSE)
-wordcloud(Data_train_corpus_clean, min.freq = 40, random.order = FALSE)
