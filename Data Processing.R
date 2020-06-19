ff031516d76d5075ab6dcb52acc2bd446dc02e2b
drshah96 <drshah96@gmail.com>
2020-06-19 12:40:44 -0400

    Data Processing: Counting Word Frequency and preparing Test and Train Dataset

--- Data Processing.R
+++ Data Processing.R
@@ -37,4 +37,30 @@ train_dtm <- DocumentTermMatrix(Data_train_corpus_clean)
 
 inspect(train_dtm)
 
+### Creating Indicator features for frequent words ###
+FreqWords <- findFreqTerms(train_dtm, 5)
 
+#Saving List using Dictionary() Function
+Dictionary <- function(x) {
+        if( is.character(x) ) {
+                return (x)
+        }
+        stop('x is not a character vector')
+}
+
+data_dict <- Dictionary(findFreqTerms(train_dtm, 5))
+
+#Appending Document Term Matrix to Train and Test Dataset 
+data_train <- DocumentTermMatrix(Data_train_corpus_clean, list(data_dict))
+data_test <- DocumentTermMatrix(Data_test_corpus_clean, list(data_dict))
+
+#Converting the frequency of word to count
+convert_counts <- function(x) {
+        x <- ifelse(x > 0, 1, 0)
+        x <- factor(x, levels = c(0, 1), labels = c("No", "Yes")) 
+        return(x)
+}
+
+#Appending count function to Train and Test Dataset
+data_train <- apply(data_train, MARGIN = 2, convert_counts)
+data_test <- apply(data_test, MARGIN = 2, convert_counts)
